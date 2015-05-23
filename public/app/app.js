477e43b3f7e4da22f88883ad44c8f6734e2e9b92
jlsears <jen.lauren.sears@gmail.com>
2015-05-23 11:39:00 -0500

    getting routes in place

---
+++ public/app/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('myapp', ['ngRoute', 'myapp.controllers']);
+
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/', {templateUrl: 'partials/index', controller: 'IndexController'})
+    .otherwise({redirectTo:'/'})
+}])
+
+.config(['$locationProvider', function($locationProvider){
+  $locationProvider.html5Mode(true);
+}]);
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,11 +4,5 @@
   angular.module('demo', [])
   .controller('MainController', ['$scope', function($scope){
     $scope.description="Film Agenda App";
-    $scope.adj='"good people watch good movies."';
-    $scope.favetheaters=[
-    {name:'Belcourt', location:'Nashville, TN', best_drink:'Cigarette Burn'},
-    {name:'Enzian', location:'Orlando, FL', best_drink:'mojito'},
-    {name:'DMAC', location:'Orlando, FL', best_drink: 'no longer applicable, unfortunately'}
-    ];
   }])
 })();
