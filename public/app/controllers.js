7a848e34c16ae5b2062596cc160e5ab9760295a4
jlsears <jen.lauren.sears@gmail.com>
2015-05-23 13:42:49 -0500

    Home About Needs Combo

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -2,4 +2,12 @@ var controllers = angular.module('myapp.controllers', []);
 
 controllers.controller('IndexController', ['$scope', function($scope){
   $scope.message = 'Hello I am the HOME PAGE';
-}])
+}]);
+
+controllers.controller('AboutController', ['$scope', function($scope){
+  $scope.message = 'Hello I am the ABOUT PAGE';
+}]);
+
+controllers.controller('NeedsController', ['$scope', function($scope){
+  $scope.message = 'Hello I am the NEEDS PAGE';
+}]);
